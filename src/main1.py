1a32ec8f2e202b6ecb5a22bcf03c74d24661e130
IamHitori <demiangba33@gmail.com>
2021-08-06 18:40:07 -0500

    Pythoncurso

--- src/main1.py
+++ src/main1.py
@@ -5,3 +5,34 @@ Los comentarios en python son una forma de organizar o dar
 una indicacion en el codigo sin afectarlo , se usa mas los
 comentarios para organizar y dar una intruccion
 '''
+
+#Numeros
+
+n = 10
+n1 = 3.4
+
+#Operadores Logicos
+
+#suma
+print(3+4)
+
+#resta
+print(3-2)
+
+#multiplicacion
+print(3*3)
+
+#divicion
+print(3/3)
+
+#reciduo
+print(2%4)
+
+#Solo entero
+print(3.5//3.5)
+
+
+#Variables
+
+n = 1
+n2 = 3.4
