590a9536bbab36af12986bce06f11ed78d90a910
IamHitori <demiangba33@gmail.com>
2021-08-06 18:29:11 -0500

    Pythoncurso

---
+++ src/main1.py
@@ -0,0 +1,7 @@
+#Comentarios
+
+'''
+Los comentarios en python son una forma de organizar o dar
+una indicacion en el codigo sin afectarlo , se usa mas los
+comentarios para organizar y dar una intruccion
+'''
